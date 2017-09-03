7c2587231688ee6e69e96e7125ef3c21be966c67
Genieliu <feixiangljs@gmail.com>
2017-09-03 10:31:34 +0800

    add comments and iris data

--- kNN.py
+++ kNN.py
@@ -6,11 +6,13 @@
 from numpy import *
 import operator
 
+# this is a sample input data.
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0,0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+# This is main kNN do, input: testData, data, labels and K
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
@@ -26,21 +28,23 @@ def classify0(inX, dataSet, labels, k):
                              key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-def file2matrix(filename):
+# transform file data to the matrix. input: filename, properties number nx, sperator default to ,
+def file2matrix(filename, nx, sep=','):
     fr = open(filename)
     arrayOflines = fr.readlines()
     numberOfLines = len(arrayOflines)
-    returnMat = zeros((numberOfLines,4))
+    returnMat = zeros((numberOfLines,nx))
     classLabelVector = []
     index = 0
     for line in arrayOflines:
         line = line.strip()
-        listFromLine = line.split(',')
-        returnMat[index,:] = listFromLine[0:4]
+        listFromLine = line.split(sep)
+        returnMat[index,:] = listFromLine[0:nx]
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat,classLabelVector
 
+# normlize the data to 0-1
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
@@ -51,19 +55,19 @@ def autoNorm(dataSet):
     normDataSet = normDataSet/tile(ranges,(m,1))
     return normDataSet, ranges, minVals
 
-def datingClassTest():
+def irisClassTest():
     hoRatio = 0.10
-    datingDataMat, datingLabels = file2matrix('iris.txt')
-    normMat, ranges, minVals = autoNorm(datingDataMat)
+    irisDataSet, irisLabels = file2matrix('iris.txt', 4)
+    normMat, ranges, minVals = autoNorm(irisDataSet)
     m = normMat.shape[0]
     numTestVecs = int(m*hoRatio)
     errorCount = 0
     for i in range(numTestVecs):
         classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:],\
-                                    datingLabels[numTestVecs:m],3)
+                                    irisLabels[numTestVecs:m],3)
         print ("the classifier came back with: %d, the real answer is: %d"\
-                %(classifierResult, datingLabels[i]))
-        if(classifierResult != datingLabels[i]):
+                %(classifierResult, irisLabels[i]))
+        if(classifierResult != irisLabels[i]):
             errorCount += 1
     print("the total error rate is: %f" %(errorCount/float(numTestVecs)))
 
@@ -75,7 +79,7 @@ def classifyIrsi():
     petal_len = float(input("petal_len:"))
     petal_wid = float(input("petal_wid:"))
 
-    irisMat, irisLabels = file2matrix("iris.txt")
+    irisMat, irisLabels = file2matrix("iris.txt",4)
     normMat, ranges, minVals = autoNorm(irisMat)
     inArr = array([sepal_len, sepal_wid, petal_len, petal_wid])
     classifierResult = classify0((inArr-minVals)/ranges, normMat, irisLabels, 3)
@@ -86,5 +90,3 @@ def classifyIrsi():
 
 #test
 if __name__ == "__main__":
-    group, labels = kNN.createDataSet()
-    kNN.classify0([1.5,0], group, labels,3)
